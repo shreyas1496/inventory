df60064bb4f7e07b1715956ffbf153d1f5a9ddfb
Shreyas Patankar <ssp1996@gmail.com>
2022-03-01 18:23:43 +0530

    Change number

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const add = (a, b) => a + b;
 const sub = (a, b) => a - b;
 
 console.log(divide(40, 4));
-console.log(add(10, 30));
+console.log(add(10, 60));
 console.log(sub(20, 10));
 console.log(multiply(3, 4));
