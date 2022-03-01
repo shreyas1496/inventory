a7c49296b515670f3f2a5b2c7298e8bdec7970b0
Sarthak Mahulikar <mahulikarsarthak6196@gmail.com>
2022-03-01 18:35:23 +0530

    Mod Div

--- math.js
+++ math.js
@@ -1,7 +1,9 @@
 const multiply = (a, b) => {
   return a * b;
 };
-
+const mod = (a, b) => {
+  return a % b;
+}
 const divide = (a, b) => a / b;
 
-module.exports = { multiply, divide };
+module.exports = { multiply, divide, mod };
